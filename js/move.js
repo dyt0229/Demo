203f29d43526dc37807adbbe7e8fb44a535b3c9a
dyt0229 <2710112390@qq.com>
2018-11-12 22:18:01 +0800

    添加了加速减速运动以步长封装

--- js/move.js
+++ js/move.js
@@ -75,3 +75,71 @@ function linearMove3(domObj,attr,startValue,endValue,timeLong){
     },timeSpace);
     
 }
+
+//封装加速运动（改变时间间隔来加速） ====封装失败
+//参数
+//DOM元素
+//样式属性
+//起始位置
+//终止位置
+//起始时间间隔
+//终止时间间隔
+/*
+function accelerateMove(domObj,attr,startValue,endValue,startTimeSpace,endTimeSpace){
+    let step=10;
+    let currValue=startValue;
+    var currTimeSpace=startTimeSpace;
+    //变多少次
+    let count=(endValue-startValue)/step;
+    let timeSpaceInc=(startTimeSpace-endTimeSpace)/count;
+    var myTimer=null;
+    function goStep(){
+        //改变数据
+        currTimeSpace-=timeSpaceInc;
+        currValue+=step;
+
+        var isExit=false;
+        //处理边界
+        if(currValue>=endValue){
+            currValue=endValue;
+            window.clearTimeout(myTimer);
+            isExit=true;
+        }
+        //改变外观
+        domObj.style[attr]=currValue+"px";
+        if(isExit){
+            return;
+        }
+        myTimer=setTimeOut(goStep, currTimeSpace)
+    }
+}
+*/
+
+//封装加速运动（改变步长来加速） 
+//参数
+//DOM元素
+//样式属性
+//起始位置
+//终止位置
+//起始步长
+//步长每次变化长度（决定加速大小）
+//时间间隔
+function accelerateMove(domObj,attr,startValue,endValue,startStep,stepSpeed,timeSpace){
+    let step=startStep;
+    let currValue=startValue;
+
+    // let direction = startValue < endValue ? 1 : -1;
+    var myTimer=setInterval(function(){
+        step+=stepSpeed;
+        if(step<=5){
+            step=5;
+        }
+        currValue+=step;
+        if(currValue>=endValue){
+            currValue=endValue;
+            window.clearInterval(myTimer);
+        }
+        //改变外观
+        domObj.style[attr]=currValue+"px";
+    },timeSpace);
+}
